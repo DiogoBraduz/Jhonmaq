5742ebea48c67264eca463b8f44da7f78b56541f
Diogo Santos <diogo.videira7@gmail.com>
2023-11-06 22:31:26 -0300

    header pronto!

---
+++ index.js
@@ -0,0 +1,22 @@
+const menuIcons = document.querySelector(".menu_icons");
+const navMenu = document.getElementById("nav-menu");
+const closeMenu = document.querySelector(".close_menu");
+const openMenu = document.querySelector(".open_menu");
+const hamburguer = document.getElementById('hamburguer');
+
+
+menuIcons.addEventListener("click", () => {
+
+menuIcons.classList.toggle("menu_closed");
+navMenu.classList.toggle("active");
+hamburguer.classList.toggle("active");
+
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    menuIcons.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    menuIcons.classList.toggle("menu_opened");
+    menuIcons.classList.toggle("menu_closed");
+    hamburguer.classList.toggle("acive");
+}))
