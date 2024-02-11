5cfef9c81725fbe9a5bf1d3c5e25ff603bdbd1f4
Gabriel P. Santos <gabriel@7751535.com>
2024-02-11 17:59:48 +0000

    feat: page finished just missing pop up

--- index.js
+++ index.js
@@ -2,21 +2,19 @@ const menuIcons = document.querySelector(".menu_icons");
 const navMenu = document.getElementById("nav-menu");
 const closeMenu = document.querySelector(".close_menu");
 const openMenu = document.querySelector(".open_menu");
-const hamburguer = document.getElementById('hamburguer');
-
+const hamburguer = document.getElementById("hamburguer");
 
 menuIcons.addEventListener("click", () => {
+  menuIcons.classList.toggle("menu_closed");
+  navMenu.classList.toggle("active");
+  hamburguer.classList.toggle("active");
+});
 
-menuIcons.classList.toggle("menu_closed");
-navMenu.classList.toggle("active");
-hamburguer.classList.toggle("active");
-
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
     menuIcons.classList.toggle("active");
     navMenu.classList.toggle("active");
     menuIcons.classList.toggle("menu_opened");
     menuIcons.classList.toggle("menu_closed");
-    hamburguer.classList.toggle("active");
-}))
+  })
+);
